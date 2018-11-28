48476414c815d9f61dcebef1f7f5bd7baf041569
aaronandyhealy <aaronhealy2010@hotmail.com>
2018-11-28 17:04:54 +0000

    Added user input for image path

--- digitrec.py
+++ digitrec.py
@@ -8,3 +8,8 @@ import gzip
 import PIL
 from PIL import Image
 import os.path
+
+
+## Ask user for the image they want to use.
+print("Please enter the path of the image you want the model to use")
+userInput = input("")
