6b1766c4c67027b8c713d10db34ed5d9326993e2
aaronandyhealy <aaronhealy2010@hotmail.com>
2018-11-28 17:02:33 +0000

    Added imports needed for script

--- digitrec.py
+++ digitrec.py
@@ -1,3 +1,10 @@
-# -*- coding: utf-8 -*-
-print("Test")
-
+import keras as kr
+from keras.layers.core import Dense, Dropout, Activation
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import sklearn.preprocessing as pre
+import gzip
+import PIL
+from PIL import Image
+import os.path
