c004a3044d566354c3b164e704a6510393a321c5
aaronandyhealy <aaronhealy2010@hotmail.com>
2018-11-28 17:07:33 +0000

    Image prep function made and data recieved back.

--- digitrec.py
+++ digitrec.py
@@ -19,6 +19,23 @@ def inputImage(userImg):
     ## Resize the image
     img = im.resize((28, 28), Image.BICUBIC)
 
+    ## Pass the image into the imagePrepare function
+    img = imageprepare(im)
+
+
+def imageprepare(im):
+
+    ## Get the data from the image
+    imd = list(im.getdata())
+
+    ## Normalize the pixels to 0 and 1.
+    imd = [(255 - x) * 1.0 / 255.0 for x in imd]
+    
+    ## Reshape the image
+    imd = np.array(list(imd)).reshape(1, 784)
+
+    ## Return the image data
+    return imd
 
 
 ## Ask user for the image they want to use.
