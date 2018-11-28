bf5e03ad6d87869383564acec882f8305c5d1973
aaronandyhealy <aaronhealy2010@hotmail.com>
2018-11-28 17:06:40 +0000

    Image resize and read in function created

--- digitrec.py
+++ digitrec.py
@@ -10,6 +10,20 @@ from PIL import Image
 import os.path
 
 
+
+def inputImage(userImg):
+
+    ## Read in an image
+    im = Image.open(userImg).convert('L')
+
+    ## Resize the image
+    img = im.resize((28, 28), Image.BICUBIC)
+
+
+
 ## Ask user for the image they want to use.
 print("Please enter the path of the image you want the model to use")
 userInput = input("")
+
+## Pass this image into the inputImage function.
+inputImage(userInput)
